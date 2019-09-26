e7140c73e2d86a7e0dd6c60fec5d548067073764
MonkeyBLuffy <MonkeyBLuffy@outlook.com>
2019-09-27 00:35:22 +0800

    增加“我的界面”以及“查询”

---
+++ README.md
@@ -0,0 +1,2 @@
+# wechat_db_movie
+豆瓣电影微信小程序实现
---
+++ pages/movie/movie.js
@@ -0,0 +1,107 @@
+var util = require('../../utils/util.js')
+Page({
+  data: {
+    currentTab: 0,
+    winWidth: 0,
+    winHeight: 0,
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    imgUrls: [
+      "images/hb1.jpg",
+      "images/hb2.jpg",
+      "images/hb3.jpg",
+    ]
+  },
+  onLoad: function (e) {
+    var page = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        console.log(res);
+        page.setData({ winWidth: res.windowWidth });
+        page.setData({ winHeight: res.windowHeight });
+      }
+    });
+    this.loadMovies();
+  },
+  switchNav: function (e) {
+    var id = e.currentTarget.id;
+    this.setData({ currentTab: id });
+  },
+
+  have1: function () {
+    wx.navigateTo({
+      url: '../have1/have1',
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function () {
+        //fail
+      },
+      complete: function () {
+        //complete
+      }
+    })
+  },
+
+  have2: function () {
+    wx.navigateTo({
+      url: '../have2/have2',
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function () {
+        //fail
+      },
+      complete: function () {
+        //complete
+      }
+    })
+  },
+
+  change: function () {
+    wx.navigateTo({
+      url: '../change/change',
+      success: function (res) {
+        console.log(res);
+      },
+      fail: function () {
+        //fail
+      },
+      complete: function () {
+        //complete
+      }
+    })
+  },
+
+
+  loadMovies: function () {
+    var page = this;
+    var key = "0df993c66c0c636e29ecbb5344252a4a";
+    wx.request({
+      url: 'https://api.douban.com/v2/movie/nowplaying?apikey=' + key, method: 'GET',
+      header: {
+        "Content-Type": "json"
+      },
+      success: function (res) {
+        console.log(res);
+        var subjects = res.data.entries;
+        console.log(subjects)
+        var size = subjects.length;//电影总数量
+        var len = parseInt(size / 3);//每行放置3个电影，计算出需要多少行
+        console.log(len);
+        console.log(subjects);
+        page.setData({ movies: subjects });
+        page.setData({ winHeight: (len + 1) * 230 });//动态的设置电影内容的高度
+      }
+    })
+  },
+  loadDetail: function (e) {
+    var id = e.currentTarget.id;
+    console.log(id)
+    wx.navigateTo({
+      url: '../movieDetail/movieDetail?id=' + id
+    })
+  }
+})
